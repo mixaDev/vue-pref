6af575078c4187ebd9f64421b18c6dd234fd236f
Михайло Голубовський <lamerumiata@gmail.com>
2017-08-11 18:32:13 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      v-model="drawer"
+      enable-resize-watcher
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-btn
+        icon
+        light
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        light
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </main>
+    <v-navigation-drawer
+      temporary
+      right
+      v-model="rightDrawer"
+    >
+      <!--<v-list>-->
+        <!--<v-list-tile>-->
+          <!--<v-list-tile-action>-->
+          <!--</v-list-tile-action>-->
+          <!--<v-list-tile-title></v-list-tile-title>-->
+        <!--</v-list-tile>-->
+      <!--</v-list>-->
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+        miniVariant: false,
+        rightDrawer: false,
+        items: [
+//          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        title: 'Pref'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
